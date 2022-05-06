60cb93ac79b244ec913a09e25045e0b1bd61208b
Tu Duan <uuxinhxinh2701@gmail.com>
2022-05-06 22:11:45 +0700

    commit

--- routes/admin.js
+++ routes/admin.js
@@ -11,10 +11,15 @@ const products = [];
 // [GET] /admin/add-product
 router.get('/add-product', adminController.getAddProduct);
 
-// [GET] /admin/p
+// [GET] /admin/products
 router.get('/products', adminController.getProducts);
 
 // [POST] /admin/add-product
 router.post('/add-product', adminController.postAddProduct);
+
+// [GET] /admin/edit-product/id
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+
 
 module.exports = router;
