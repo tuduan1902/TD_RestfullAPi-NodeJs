4e3f4878f5ebff70ca621c6937d66e258df8b1be
Tu Duan <uuxinhxinh2701@gmail.com>
2022-06-06 22:31:02 +0700

    Connect database mysql

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorControllers = require('./controllers/error');
+const db = require('./util/database');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+db.execute('SELECT * FROM products');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
